41321d71583d55c52644a7750f75440296b38f79
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-07-02 19:19:52 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import UserDetailsComponent from './Components/UserDashboard/UserDetailsComponent';
-import ImageComponent from './Components/UserDashboard/ImageComponent';
 import Navigator from './Components/UserDashboard/NavigationBar';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import WebcamComponent from './Components/UserDashboard/WebcamComponent';
 import VideoComponent from './Components/UserDashboard/VideoComponent';
-import FaceCombinedComponent from './Components/UserDashboard/FaceCombinedComponent';
 import MultipleImageComponent from './Components/UserDashboard/MultipleImageComponent';
-import LicenseCombinedComponent from './Components/UserDashboard/LicenseCombinedComponent';
 import NewFace from './Components/UserDashboard/NewFaceRecognition';
 import NewLicense from './Components/UserDashboard/NewLicense';
 import UserForm  from './Components/SignupForm/UserForm';
